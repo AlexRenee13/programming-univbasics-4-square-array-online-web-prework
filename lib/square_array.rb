1bd890916ebedd21d6e6b7f8fc863f9fc03483b8
Alexis Palacios <alexis1994662@yahoo.com>
2019-10-28 06:58:36 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -5,11 +5,11 @@
 #   end
 # end
 
-def square_array(array)
- new_array = [1, 2, 3]
-  count = 0
-  while count < array.length do
-   new_array<< array.each **2
-  end
-    new_array
-end
+# def square_array(array)
+# new_array = [1, 2, 3]
+#   count = 0
+#   while count < array.length do
+#   new_array<< array.each **2
+#   end
+#     new_array
+# end
